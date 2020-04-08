24be84d609354d7102bcdd5e194ff6f291361b84
JameyWoo <2622075127wjh@gmail.com>
2020-04-09 00:34:59 +0800

    format file

--- ThreadPoolCpp11/ThreadPool.h
+++ ThreadPoolCpp11/ThreadPool.h
@@ -1,137 +1,121 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
-#include <thread>
-#include <mutex>
 #include <atomic>
 #include <condition_variable>
 #include <functional>
 #include <future>
+#include <mutex>
 #include <queue>
+#include <thread>
 #include <type_traits>
 #include <utility>
 #include <vector>
 
 // C++11 版的 线程池
-namespace zl
-{
-    class ThreadsGuard
-    {
-    public:
-        ThreadsGuard(std::vector<std::thread>& v)
-            : threads_(v)
-        {
-            
-        }
+namespace zl {
+class ThreadsGuard {
+   public:
+	ThreadsGuard(std::vector<std::thread>& v)
+		: threads_(v) {
+	}
 
-        ~ThreadsGuard()
-        {
-            for (size_t i = 0; i != threads_.size(); ++i)
-            {
-                if (threads_[i].joinable())
-                {
-                    threads_[i].join();
-                }
-            }
-        }
-    private:
-        ThreadsGuard(ThreadsGuard&& tg) = delete;
-        ThreadsGuard& operator = (ThreadsGuard&& tg) = delete;
+	~ThreadsGuard() {
+		for (size_t i = 0; i != threads_.size(); ++i) {
+			if (threads_[i].joinable()) {
+				threads_[i].join();
+			}
+		}
+	}
 
-        ThreadsGuard(const ThreadsGuard&) = delete;
-        ThreadsGuard& operator = (const ThreadsGuard&) = delete;
-    private:
-        std::vector<std::thread>& threads_;
-    };
+   private:
+	ThreadsGuard(ThreadsGuard&& tg) = delete;
+	ThreadsGuard& operator=(ThreadsGuard&& tg) = delete;
 
+	ThreadsGuard(const ThreadsGuard&) = delete;
+	ThreadsGuard& operator=(const ThreadsGuard&) = delete;
 
-    class ThreadPool
-    {
-    public:
-        typedef std::function<void()> task_type;
+   private:
+	std::vector<std::thread>& threads_;
+};
 
-    public:
-        explicit ThreadPool(int n = 0);
+class ThreadPool {
+   public:
+	typedef std::function<void()> task_type;
 
-        ~ThreadPool()
-        {
-            stop();
-            cond_.notify_all();
-        }
+   public:
+	explicit ThreadPool(int n = 0);
 
-        void stop()
-        {
-            stop_.store(true, std::memory_order_release);
-        }
+	~ThreadPool() {
+		stop();
+		cond_.notify_all();
+	}
 
-        template<class Function, class... Args>
-        std::future<typename std::result_of<Function(Args...)>::type> add(Function&&, Args&&...);
+	void stop() {
+		stop_.store(true, std::memory_order_release);
+	}
 
-    private:
-        ThreadPool(ThreadPool&&) = delete;
-        ThreadPool& operator = (ThreadPool&&) = delete;
-        ThreadPool(const ThreadPool&) = delete;
-        ThreadPool& operator = (const ThreadPool&) = delete;
+	template <class Function, class... Args>
+	std::future<typename std::result_of<Function(Args...)>::type> add(Function&&, Args&&...);
 
-    private:
-        std::atomic<bool> stop_;
-        std::mutex mtx_;
-        std::condition_variable cond_;
+   private:
+	ThreadPool(ThreadPool&&) = delete;
+	ThreadPool& operator=(ThreadPool&&) = delete;
+	ThreadPool(const ThreadPool&)		= delete;
+	ThreadPool& operator=(const ThreadPool&) = delete;
 
-        std::queue<task_type> tasks_;
-        std::vector<std::thread> threads_;
-        zl::ThreadsGuard tg_;
-    };
+   private:
+	std::atomic<bool> stop_;
+	std::mutex mtx_;
+	std::condition_variable cond_;
 
+	std::queue<task_type> tasks_;
+	std::vector<std::thread> threads_;
+	zl::ThreadsGuard tg_;
+};
 
-    inline ThreadPool::ThreadPool(int n)
-        : stop_(false)
-        , tg_(threads_)
-    {
-        int nthreads = n;
-        if (nthreads <= 0)
-        {
-            nthreads = std::thread::hardware_concurrency();
-            nthreads = (nthreads == 0 ? 2 : nthreads);
-        }
+inline ThreadPool::ThreadPool(int n)
+	: stop_(false), tg_(threads_) {
+	int nthreads = n;
+	if (nthreads <= 0) {
+		nthreads = std::thread::hardware_concurrency();
+		nthreads = (nthreads == 0 ? 2 : nthreads);
+	}
 
-        for (int i = 0; i != nthreads; ++i)
-        {
-            threads_.push_back(std::thread([this]{
-                while (!stop_.load(std::memory_order_acquire))
-                {
-                    task_type task;
-                    {
-                        std::unique_lock<std::mutex> ulk(this->mtx_);
-                        this->cond_.wait(ulk, [this]{ return stop_.load(std::memory_order_acquire) || !this->tasks_.empty(); });
-                        if (stop_.load(std::memory_order_acquire))
-                            return;
-                        task = std::move(this->tasks_.front());
-                        this->tasks_.pop();
-                    }
-                    task();
-                }
-            }));
-        }
-    }
-
-    template<class Function, class... Args>
-    std::future<typename std::result_of<Function(Args...)>::type>
-        ThreadPool::add(Function&& fcn, Args&&... args)
-    {
-        typedef typename std::result_of<Function(Args...)>::type return_type;
-        typedef std::packaged_task<return_type()> task;
-
-        auto t = std::make_shared<task>(std::bind(std::forward<Function>(fcn), std::forward<Args>(args)...));
-        auto ret = t->get_future();
-        {
-            std::lock_guard<std::mutex> lg(mtx_);
-            if (stop_.load(std::memory_order_acquire))
-                throw std::runtime_error("thread pool has stopped");
-            tasks_.emplace([t]{(*t)(); });
-        }
-        cond_.notify_one();
-        return ret;
-    }
+	for (int i = 0; i != nthreads; ++i) {
+		threads_.push_back(std::thread([this] {
+			while (!stop_.load(std::memory_order_acquire)) {
+				task_type task;
+				{
+					std::unique_lock<std::mutex> ulk(this->mtx_);
+					this->cond_.wait(ulk, [this] { return stop_.load(std::memory_order_acquire) || !this->tasks_.empty(); });
+					if (stop_.load(std::memory_order_acquire))
+						return;
+					task = std::move(this->tasks_.front());
+					this->tasks_.pop();
+				}
+				task();
+			}
+		}));
+	}
 }
 
-#endif  /* THREAD_POOL_H */
+template <class Function, class... Args>
+std::future<typename std::result_of<Function(Args...)>::type>
+ThreadPool::add(Function&& fcn, Args&&... args) {
+	typedef typename std::result_of<Function(Args...)>::type return_type;
+	typedef std::packaged_task<return_type()> task;
+
+	auto t   = std::make_shared<task>(std::bind(std::forward<Function>(fcn), std::forward<Args>(args)...));
+	auto ret = t->get_future();
+	{
+		std::lock_guard<std::mutex> lg(mtx_);
+		if (stop_.load(std::memory_order_acquire))
+			throw std::runtime_error("thread pool has stopped");
+		tasks_.emplace([t] { (*t)(); });
+	}
+	cond_.notify_one();
+	return ret;
+}
+}  // namespace zl
+
+#endif /* THREAD_POOL_H */
